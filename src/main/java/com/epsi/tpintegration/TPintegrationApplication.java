4a9b674fdb032dc550795ffc0c4e4b4ceb7a52e5
Toni59400 <tonipira.tp@gmail.com>
2024-02-21 10:19:27 +0100

    ee
    ee

---
+++ src/main/java/com/epsi/tpintegration/TPintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.tpintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TPintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TPintegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epsi/tpintegration/TPintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsi.tpintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TPintegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
