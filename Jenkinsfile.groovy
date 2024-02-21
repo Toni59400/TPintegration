9bc2921ed7a2a2557b56388232eb85084582ca3a
Toni59400 <tonipira.tp@gmail.com>
2024-02-21 10:31:52 +0100

    Create Jenkinsfile.groovy
    ff

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/Toni59400/TPintegration'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+
+        stage('SonarQube Analysis') {
+            steps {
+                withSonarQubeEnv('SonarQubeServer') {
+                    sh 'mvn sonar:sonar'
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            junit 'target/surefire-reports/**/*.xml'
+            archiveArtifacts 'target/*.jar'
+            deleteDir()
+        }
+    }
+}
